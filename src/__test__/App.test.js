9995cd6531c4de490232819039bd23b8a2190a30
Nahuel <nahuel-ramirez@hotmail.com>
2023-03-16 16:47:55 -0300

    Implementando mi primer test!

---
+++ src/__test__/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  test("The form should be render properly", () => {
+    render(<App />);
+    screen.getByText("Nombre de la tarea");
+  });
+});
